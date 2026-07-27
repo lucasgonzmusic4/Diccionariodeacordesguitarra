008453b028e76c80c0de511a6ae07165668c6884
lucasgonzmusic4 <lucasgonzmusic4@gmail.com>
2026-07-27 16:17:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,36 @@
-const CACHE_NAME = 'diccionario-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icono.png'
-];
+const CACHE_NAME = 'diccionario-v2';
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icono.png'
+      ]);
+    })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); 
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
